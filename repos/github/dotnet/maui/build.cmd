9bc7e2f5fcf614b7c03ca702415cef52d2e25b7c
Lance Collins <lancec@microsoft.com>
2023-06-10 17:39:53 -0700

    Use proper cmd comments.

--- repos/github/dotnet/maui/build.cmd
+++ repos/github/dotnet/maui/build.cmd
@@ -1,13 +1,13 @@
 #Taken from https://github.com/dotnet/maui/blob/main/.github/DEVELOPMENT.md
 
-# install local tools required to build (cake, pwsh, etc..)
+REM install local tools required to build (cake, pwsh, etc..)
 dotnet tool restore
 
-# Provision .NET SDK in bin\dotnet
+REM Provision .NET SDK in bin\dotnet
 dotnet build src\DotNet\DotNet.csproj
 
-# Builds Maui MSBuild tasks
+REM Builds Maui MSBuild tasks
 .\bin\dotnet\dotnet build Microsoft.Maui.BuildTasks.slnf /bl:%BinLogDir%\Microsoft.Maui.BuildTasks.binlog
 
-# Builds the rest of Maui
+REM Builds the rest of Maui
 .\bin\dotnet\dotnet build Microsoft.Maui.sln /bl:%BinLogDir%\Microsoft.Maui.binlog
