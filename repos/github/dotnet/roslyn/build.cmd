6913e18f8f5992cd88090de3f6799a140f8d8e34
Lance Collins <lancec@microsoft.com>
2023-08-05 09:17:28 -0700

    Add auto index logic for roslyn.

---
+++ repos/github/dotnet/roslyn/build.cmd
@@ -0,0 +1,14 @@
+REM Disk space Before Build
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "%~dp0/diskspace.ps1"
+
+REM Build using arcade build logic
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "eng/build.ps1 -ci -restore -build -binaryLog /p:GeneratePkgDefFile=false /p:CreateVsixContainer=false /p:DeployExtension=false /p:IsSwixProject=false /p:UsingToolVSSDK=false"
+
+REM Disk space Post Build
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "%~dp0/diskspace.ps1"
+
+REM Copy binlogs
+xcopy "%SrcDir%\artifacts\log\*.binlog" "%BinlogDir%" /S /I /Y
+
+REM Copy binlogs to debug dir
+xcopy "%SrcDir%\artifacts\log\*.binlog" "%CodexDebugDir%" /S /I /Y
