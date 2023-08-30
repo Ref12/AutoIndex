6ef31fd2f2ad5cbbb41119cdc0a8903f36446e24
Lance Collins <lancec@microsoft.com>
2023-08-30 11:03:02 -0500

    Generate binlogs next to projects.

--- repos/.build/nukecompile.cmd
+++ repos/.build/nukecompile.cmd
@@ -3,3 +3,6 @@ move "%~dp0\Binlog.Directory.build.rsp" "%SrcDir%\..\Directory.build.rsp"
 
 REM Call build
 call "%SrcDir%\build.cmd" Compile
+
+REM Copy binlogs
+xcopy "%SrcDir%\*.binlog" "%BinlogDir%" /S /I /Y
