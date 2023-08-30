a05eb8d90c582a4e96ea409610320f8cf8b49fac
Lance Collins <lancec@microsoft.com>
2023-08-30 00:36:55 -0500

    Add logic for specifying binlog arg vs rsp file.

--- repos/github/AngleSharp/AngleSharp/build.cmd
+++ repos/github/AngleSharp/AngleSharp/build.cmd
@@ -1,2 +1,7 @@
-REM Build and emit binlog
-call "%SrcDir%\build.cmd" --BinaryLog %BinLogDir%\build.binlog
+REM Add Directory.build.rsp which specifies that binlog should be produced
+move "..\..\..\.build\Binlog.Directory.build.rsp" "%SrcDir%\..\Directory.build.rsp"
+
+dir "%SrcDir%\.."
+
+REM Call build
+call "%SrcDir%\build.cmd"
