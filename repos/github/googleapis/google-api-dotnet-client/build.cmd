a49739abe20224d0d999c29b793854e36c7adfbb
Lance Collins <lancec@microsoft.com>
2023-09-13 10:42:16 -0500

    Add solution build targets to build.cmd

--- repos/github/googleapis/google-api-dotnet-client/build.cmd
+++ repos/github/googleapis/google-api-dotnet-client/build.cmd
@@ -1,3 +1,7 @@
+dotnet build "%~dp0\build.proj" /t:RestoreSolutions "/bl:%CodexDebugDir%\restore.sln.binlog"
+
+dotnet build "%~dp0\build.proj" /t:BuildSolutions "/bl:%BinlogDir%\build.sln.binlog"
+
 dotnet build "%~dp0\build.proj" "/bl:%BinLogDir%\build.binlog"
 
 xcopy "%BinlogDir%\*.binlog" "%CodexDebugDir%\" /S /I /Y /F
