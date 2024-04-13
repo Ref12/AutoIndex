b0727d318304ae609c6371c936afa235ac6843f9
Lance Collins <lancec@microsoft.com>
2024-04-13 14:20:09 -0700

    Run buildxl analyzer

--- repos/github/microsoft/buildxl/build.cmd
+++ repos/github/microsoft/buildxl/build.cmd
@@ -1,1 +1,15 @@
-call %SrcDir%\bxl.cmd
+set BUILDXL_LOG_DIR=%CodexDebugDir%\bxllogs
+
+set CSARGS_OUTPUT_DIR==%CodexDebugDir%\csargs
+
+echo ----- Running BuildXL build -----
+
+call %SrcDir%\bxl.cmd /f:tag='compile' /q:Debug /incrementalScheduling- /enableLazyOutputs- /logsDirectory:%BUILDXL_LOG_DIR%
+
+echo ----- Running Codex execution analyzer -----
+
+call %SrcDir%\bxlanalyzer.cmd /mode:Codex /xl:%BUILDXL_LOG_DIR% /o:%CSARGS_OUTPUT_DIR%
+
+echo ----- Running Codex script analyzer -----
+
+REM call %SrcDir%\bxlanalyzer.cmd /mode:Codex /xl:%BUILDXL_LOG_DIR% /o:%CSARGS_OUTPUT_DIR%
