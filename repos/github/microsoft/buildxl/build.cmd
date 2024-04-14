ec337d3a0b09bee3b731b7d27427298fce544a8c
Lance Collins <lancec@microsoft.com>
2024-04-13 22:31:26 -0700

    Use caching in buildxl build.

--- repos/github/microsoft/buildxl/build.cmd
+++ repos/github/microsoft/buildxl/build.cmd
@@ -1,10 +1,20 @@
 set BUILDXL_LOG_DIR=%CodexDebugDir%\bxllogs
 
+echo ----- Writing cache config -----
+
+set BUILDXL_LOCAL_CACHE_DIRECTORY=%CodexBuildTempDir%/bxlcache
+set BUILDXL_REMOTE_CACHE_DIRECTORY=Z:/bxlcache
+
+set BXL_CACHE_CONFIG_PATH="%~dp0/writeconfig.ps1"
+
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command %BXL_CACHE_CONFIG_PATH%
+
 set CSARGS_OUTPUT_DIR==%CodexDebugDir%\csargs
 
 echo ----- Running BuildXL build -----
 
-call %SrcDir%\bxl.cmd /f:tag='compile' /q:Debug /incrementalScheduling- /enableLazyOutputs- /logsDirectory:%BUILDXL_LOG_DIR%
+call %SrcDir%\bxl.cmd -DoNotUseDefaultCacheConfigFilePath /f:tag='compile' /q:Debug /incrementalScheduling- /enableLazyOutputs- /logsDirectory:%BUILDXL_LOG_DIR% /logExecution ^
+/cacheConfigFilePath:%BXL_CACHE_CONFIG_PATH%
 
 echo ----- Running Codex execution analyzer -----
 
---
+++ repos/github/microsoft/buildxl/writeconfig.ps1
@@ -0,0 +1,54 @@
+$configOptions = Get-CacheConfig;
+
+if (!(Test-Path $env:BUILDXL_LOCAL_CACHE_DIRECTORY)) {
+    New-Item -Path $env:BUILDXL_LOCAL_CACHE_DIRECTORY -ItemType Directory
+}
+
+Set-Content -Path "$env:BUILDXL_LOCAL_CACHE_DIRECTORY/config.json" -Value (ConvertTo-Json $configOptions)
+
+function Get-CacheConfig {
+    $localCache = @{
+        Assembly                   = "BuildXL.Cache.MemoizationStoreAdapter";
+        Type                       = "BuildXL.Cache.MemoizationStoreAdapter.MemoizationStoreCacheFactory";
+        CacheId                    = "SelfhostCS2L1";
+        MaxCacheSizeInMB           = 40480;
+        CacheRootPath              = $env:BUILDXL_LOCAL_CACHE_DIRECTORY;
+        CacheLogPath               = "[BuildXLSelectedLogPath]";
+        UseStreamCAS               = $true;
+        UseRocksDbMemoizationStore = $true;
+    };
+
+    if (!$env:BUILDXL_REMOTE_CACHE_DIRECTORY) {
+        return $localCache;
+    }
+
+    if (!(Test-Path $env:BUILDXL_REMOTE_CACHE_DIRECTORY)) {
+        New-Item -Path $env:BUILDXL_REMOTE_CACHE_DIRECTORY -ItemType Directory
+    }
+
+    $remoteCache = @{
+        Assembly                   = "BuildXL.Cache.MemoizationStoreAdapter";
+        Type                       = "BuildXL.Cache.MemoizationStoreAdapter.MemoizationStoreCacheFactory";
+        CacheId                    = "SelfhostCS2L1";
+        MaxCacheSizeInMB           = 40480;
+        CacheRootPath              = $env:BUILDXL_REMOTE_CACHE_DIRECTORY;
+        CacheLogPath               = "[BuildXLSelectedLogPath]";
+        UseStreamCAS               = $true;
+        UseRocksDbMemoizationStore = $true;
+    };
+
+    $resultCache = @{
+        Assembly                              = "BuildXL.Cache.VerticalAggregator";
+        Type                                  = "BuildXL.Cache.VerticalAggregator.VerticalCacheAggregatorFactory";
+        RemoteIsReadOnly                      = $false;
+        RemoteContentIsReadOnly               = $false;
+        WriteThroughCasData                   = $true;
+        LocalCache                            = $localCache;
+        RemoteCache                           = $remoteCache;
+        RemoteConstructionTimeoutMilliseconds = 36000;
+        SkipDeterminismRecovery               = $true;
+        FailIfRemoteFails                     = $true;
+    };
+
+    return $resultCache;
+}
