e3f266a353e79967c8181c47e7dbf1c5050810cd
Lance Collins <lancec@microsoft.com>
2024-07-27 15:49:55 -0700

    Use Constrain what is built.

--- repos/github/dotnet/runtime/build.cmd
+++ repos/github/dotnet/runtime/build.cmd
@@ -2,7 +2,7 @@ REM Disk space Before Build
 powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "%CodexBuildConfigRoot%/diskspace.ps1"
 
 REM Build using arcade build logic
-powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "eng/build.ps1 -ci -restore -build -binaryLog
+build.cmd clr+libs -rc Debug -binaryLog
 
 REM Disk space Post Build
 powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "%CodexBuildConfigRoot%/diskspace.ps1"
