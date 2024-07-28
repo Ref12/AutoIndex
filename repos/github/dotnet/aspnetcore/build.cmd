a1d7facdc17cf9c5f191cba0e066fddd0d7db404
Lance Collins <lancec@microsoft.com>
2024-07-27 20:23:24 -0700

    Use same args as ci build in aspnetcore.

--- repos/github/dotnet/aspnetcore/build.cmd
+++ repos/github/dotnet/aspnetcore/build.cmd
@@ -1,9 +1,9 @@
-call npm install -g node
+REM call npm install -g node
 
-call "%SrcDir%\restore.cmd"
+REM call "%SrcDir%\restore.cmd"
 
 REM Build using arcade build logic
 
-call "%SrcDir%\eng\build.cmd" -buildManaged -binarylog -arch x64
+call "%SrcDir%\eng\build.cmd" -ci -prepareMachine -binarylog -arch x64
 
 call "%CodexBuildConfigRoot%\arcade.copybinlogs.cmd"
