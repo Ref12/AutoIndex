5d9650803ae1b741a80c0f83f8f4f99e25f14629
Lance Collins <lancec@microsoft.com>
2024-08-20 14:21:03 -0700

    Print dotnet version

--- repos/.build/dotnet-install.cmd
+++ repos/.build/dotnet-install.cmd
@@ -1,3 +1,5 @@
 curl -o "%CodexBuildTempDir%\dotnet-install.ps1" "https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.ps1"
 
 powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "%CodexBuildTempDir%\dotnet-install.ps1 %*"
+
+dotnet --version
